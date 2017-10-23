dafacbdc2978e18f6d7e40c8617bbe11e0197a66
Rokara <33036054+Rokara@users.noreply.github.com>
2017-10-23 19:58:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-const Discord = require ('Discord.js');
+const Discord = require('Discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('Sloužím vlasti!');
 });
 
-client.on('message', message =>{
+client.on('message', message => {
   If (message.content === 'ping') {
       message.reply('pong, vole!');
      }
  });
- client.login(process.env.MzcxODk1NTc1NzQ0MDg2MDI4.DM-t5g.CDI4TYnuFJ54rsqmZNOOxpjHQhM)
+ client.login(process.env.'MzcxODk1NTc1NzQ0MDg2MDI4.DM-t5g.CDI4TYnuFJ54rsqmZNOOxpjHQhM')
