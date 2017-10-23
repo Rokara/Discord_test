daa2f661be7557effd9ecde3636b02c0827a21dc
Rokara <33036054+Rokara@users.noreply.github.com>
2017-10-23 22:10:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const PurgePrikaz = '!!BurnHereticBurn!!'; 
+const PurgePrikaz = '!!BURN*HERETIC*BURN!!'; 
 
 client.on('ready', () => {
     console.log('Sloužím vlasti!');
